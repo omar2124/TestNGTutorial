d9a5cbc154d38b91ee23f2fb2048e856c660e6e5
omar <omar_2124@hotmail.com>
2019-03-12 23:01:31 -0600

    Working with listeners

---
+++ src/Test/Day8.java
@@ -0,0 +1,19 @@
+package Test;
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day8 {
+
+	@Test
+	public void LastExecution()
+	{
+		System.out.println("This is the last execution of the package Test");
+		Assert.assertTrue(false);
+		
+	}
+	
+	
+}
